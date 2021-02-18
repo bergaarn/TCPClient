0deae029b90141505978d1ede1565820e9d33439
Joel Berg <joel.berg@hotmail.se>
2021-02-18 02:02:25 +0100

    Finalized the assignment and checked for errors

--- clientmain.cpp
+++ clientmain.cpp
@@ -11,13 +11,12 @@
 
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass 
-//#define DEBUG
-
+#define DEBUG
 #define BUFFERSIZE 100
 #define PROTOCOL "TEXT TCP 1.0"
 
 // Included to get the support library
-#include "calcLib.h"
+#include <calcLib.h>
 
 int main(int argc, char *argv[]){
 
@@ -25,17 +24,23 @@ int main(int argc, char *argv[]){
   int recvBytes;
   int socketFD;
   char recvBuffer[BUFFERSIZE];
-  char adressBuffer[INET_ADDRSTRLEN];
+  char addressBuffer[INET_ADDRSTRLEN];
   char* terminalIP;
   char* terminalPort;
   char delim[] = ":";
   struct addrinfo hints;
   struct addrinfo* server;
-  struct addrinfo* adressPointer;
+  struct addrinfo* addressPointer;
   
   if(argc != 2)
   {
     fprintf(stderr, "Program call was incorrect.\n");
+    return 1;
+  }
+
+  if(strlen(argv[1]) < 13)
+  {
+    fprintf(stderr, "No Port Found.\n");
     return 1;
   }
 
@@ -54,15 +59,15 @@ int main(int argc, char *argv[]){
     return 2;
   }
 
-  for(adressPointer = server; adressPointer != NULL; adressPointer = adressPointer->ai_next)
+  for(addressPointer = server; addressPointer != NULL; addressPointer = addressPointer->ai_next)
   {
-    socketFD = socket(adressPointer->ai_family, adressPointer->ai_socktype, adressPointer->ai_protocol);
+    socketFD = socket(addressPointer->ai_family, addressPointer->ai_socktype, addressPointer->ai_protocol);
     if (socketFD == -1)
     {
       continue;
     }
 
-    if(connect(socketFD, adressPointer->ai_addr, adressPointer->ai_addrlen) == -1)
+    if(connect(socketFD, addressPointer->ai_addr, addressPointer->ai_addrlen) == -1)
     {
       close(socketFD);
       continue;
@@ -71,13 +76,27 @@ int main(int argc, char *argv[]){
     break;
   }
 
-  if(adressPointer == NULL)
+  if(addressPointer == NULL)
   {
     fprintf(stderr, "Failed to connect to a server.\n");
     return 3;
   }
 
-  inet_ntop(adressPointer->ai_family, adressPointer->ai_addr, adressBuffer, sizeof adressBuffer);
+  else
+  {
+    char localIP[INET_ADDRSTRLEN];
+    struct sockaddr_in myAddress;
+    memset(&myAddress, 0, sizeof(myAddress));
+    int myLength = sizeof(myAddress);
+
+    getsockname(socketFD, (struct sockaddr*)&myAddress, &myAddress.sin_addr.s_addr);
+    inet_ntop(AF_INET, &myAddress.sin_addr.s_addr, localIP, sizeof(localIP));
+    
+    printf("Connected to Server. My Local IP is: %s\n", localIP);
+ 
+  }
+
+  inet_ntop(addressPointer->ai_family, addressPointer->ai_addr, addressBuffer, sizeof addressBuffer);
   freeaddrinfo(server);
 
   recvBytes = recv(socketFD, recvBuffer, BUFFERSIZE-1, 0);
@@ -113,7 +132,6 @@ int main(int argc, char *argv[]){
   }
   printf("%s\n", recvBuffer);
   
-  // Dissect String
   char* operand;
   char* firstValue;
   char* secondValue;
@@ -125,7 +143,6 @@ int main(int argc, char *argv[]){
  
   if(operand[0] == 'f')
   {
-    // float values
     double firstFloatValue = atof(firstValue);
     double secondFloatValue = atof(secondValue);
     double floatResult;
@@ -137,7 +154,7 @@ int main(int argc, char *argv[]){
     }
     else if(operand[1] == 'd')
     {
-      floatResult = firstFloatValue - secondFloatValue;
+      floatResult = firstFloatValue / secondFloatValue;
       printf("%8.8g\n", floatResult);
     }
 
@@ -149,7 +166,7 @@ int main(int argc, char *argv[]){
 
     else if(operand[1] == 's')
     {
-      floatResult = firstFloatValue / secondFloatValue;
+      floatResult = firstFloatValue - secondFloatValue;
       printf("%8.8g\n", floatResult);
     }
     else
@@ -163,10 +180,8 @@ int main(int argc, char *argv[]){
     int floatLength = strlen(floatBuffer);
     send(socketFD, floatBuffer, floatLength, 0);
   }
-
   else 
   {
-    // integers
     int firstIntValue = atoi(firstValue);
     int secondIntValue = atoi(secondValue);
     int intResult;
@@ -205,6 +220,7 @@ int main(int argc, char *argv[]){
     int intLength = strlen(intBuffer);
     send(socketFD, intBuffer, intLength, 0);
   }
+
   memset(recvBuffer, 0, sizeof(recvBuffer));
   recvBytes = recv(socketFD, recvBuffer, BUFFERSIZE-1, 0);
   if (recvBytes == -1)
@@ -212,12 +228,7 @@ int main(int argc, char *argv[]){
     return 9;
   }
   printf("%s\n", recvBuffer);
-
-  // Receive OK or ERROR
-  //printf("%s %d %d\n", operand, firstIntValue, secondIntValue);
-  //printf("%s %8.8g %8.8g\n", operand, firstFloatValue, secondFloatValue);
-
-
+  
   close(socketFD);
 
   #ifdef DEBUG 
